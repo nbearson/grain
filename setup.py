b641e62f8f2932b584f014e469d665908e469140
nickb <nickb@ssec.wisc.edu>
2015-01-09 00:47:19 -0600

    updated version number to reflect the new leap-seconds file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='grain',
-      version='1.0',
+      version='1.0.1',
       description='A super simple UTC<->TAI conversion package',
       author='Nick Bearson',
       author_email='nickb@ssec.wisc.edu',
