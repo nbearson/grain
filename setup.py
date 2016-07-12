e6f74f0b5b8cb298b7fc43d838871f2859ab2d98
nickb <nickb@ssec.wisc.edu>
2016-07-12 16:42:12 -0500

    adding a notion of 'last update' to the version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='grain',
-      version='1.0.1',
+      version='1.0.2016-07',
       description='A super simple UTC<->TAI conversion package',
       author='Nick Bearson',
       author_email='nickb@ssec.wisc.edu',
