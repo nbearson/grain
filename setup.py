90afedb5cf560a5798298546cf3eeba50205f84f
Nick Bearson <nickb@ssec.wisc.edu>
2019-09-17 11:14:14 -0500

    adds GPLv3 as the license

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='grain',
-      version='1.0.2019-01',
+      version='1.0.2019-09',
       description='A super simple UTC<->TAI conversion package',
       author='Nick Bearson',
       author_email='nickb@ssec.wisc.edu',
+      license='GPLv3',
       packages=find_packages('.'),
       install_requires=[],
       include_package_data = True,
